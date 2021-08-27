3e9be0577e27973b6b9cc3954b3734a61a9f0c24
Umid Karimov <Umid.Karimov@unibank.az>
2021-08-27 14:35:02 +0400

    add simple controller

--- src/main/java/demo/config/WebConfig.java
+++ src/main/java/demo/config/WebConfig.java
@@ -1,6 +1,7 @@
 package demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "demo.controller")
 public class WebConfig {
     @Bean
     public ViewResolver getViewResolver () {
@@ -23,7 +25,7 @@ public class WebConfig {
     @Bean
     public FreeMarkerConfigurer getFreeMarkerConfigurer() {
             FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-            freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views");
+            freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views/");
             return freeMarkerConfigurer;
     }
 }
