7abfbee3519bac9ffde7ce44e747150c2b373bcf
Umid Karimov <Umid.Karimov@unibank.az>
2021-09-17 17:26:59 +0400

    staff

--- src/main/java/demo/config/WebAppInitializer.java
+++ src/main/java/demo/config/WebAppInitializer.java
@@ -1,2 +1,23 @@
-package demo.config;public class WebAppInitializer {
+package demo.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class,WebConfig.class);
+        context.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic dispatcher =
+        servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+    }
 }
