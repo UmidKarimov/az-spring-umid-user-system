27745b7a9810144d6c014dd5a7f89bcaedea8e1f
Umid Karimov <Umid.Karimov@unibank.az>
2021-09-28 10:53:32 +0400

    add people controller

---
+++ src/main/java/az/spring/umid/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package az.spring.umid.dao;
+
+
+import az.spring.umid.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNTER = 0;
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        for (int i = 0; i <= 5; i++) {
+            people.add(new Person(++PEOPLE_COUNTER, "Person No" + PEOPLE_COUNTER));
+        }
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+}
